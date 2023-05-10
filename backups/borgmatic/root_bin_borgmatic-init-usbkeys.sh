ba360e383b77360d6101fbbf35989adbd770c7d8
Thomas Harold <tgh@tgharold.com>
2023-05-10 06:37:38 -0400

    grab the borgmatic bin scripts

---
+++ backups/borgmatic/root_bin_borgmatic-init-usbkeys.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# /root/bin/borgmatic-init-usbkeys
+
+borgmatic init --encrypt repokey --config /root/borgmatic.d/usbkeys
---
+++ backups/borgmatic/root_bin_borgmatic-usbkeys.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# /root/bin/borgmatic-usbkeys
+
+borgmatic --config /root/borgmatic.d/usbkeys --verbosity 1 --stats
